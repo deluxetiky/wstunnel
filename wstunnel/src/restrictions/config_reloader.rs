560077c1b704ff33a10fd403263cc3e260e8db65
Fatih Sinan Bir <f.sinanbir@gmail.com>
2025-12-19 01:45:00 +0300

    Squash merge hotfix/restictions-reload into main

--- wstunnel/src/restrictions/config_reloader.rs
+++ wstunnel/src/restrictions/config_reloader.rs
@@ -3,13 +3,11 @@ use crate::restrictions::config_reloader::RestrictionsRulesReloaderState::{Confi
 use anyhow::Context;
 use arc_swap::ArcSwap;
 use log::trace;
-use notify::{EventKind, RecommendedWatcher, Watcher};
+use notify::{RecommendedWatcher, Watcher};
 use parking_lot::Mutex;
 use std::path::PathBuf;
 use std::sync::Arc;
-use std::thread;
-use std::time::Duration;
-use tracing::{error, info, warn};
+use tracing::{error, info};
 
 struct ConfigReloaderState {
     fs_watcher: Mutex<RecommendedWatcher>,
@@ -22,15 +20,6 @@ enum RestrictionsRulesReloaderState {
     Config(Arc<ConfigReloaderState>),
 }
 
-impl RestrictionsRulesReloaderState {
-    fn fs_watcher(&self) -> &Mutex<RecommendedWatcher> {
-        match self {
-            Static => unreachable!(),
-            Config(this) => &this.fs_watcher,
-        }
-    }
-}
-
 #[derive(Clone)]
 pub struct RestrictionsRulesReloader {
     state: RestrictionsRulesReloaderState,
@@ -41,7 +30,11 @@ impl RestrictionsRulesReloader {
     pub fn new(restrictions_rules: RestrictionsRules, config_path: Option<PathBuf>) -> anyhow::Result<Self> {
         // If there is no custom certificate and private key, there is nothing to watch
         let config_path = if let Some(config_path) = config_path {
-            config_path
+            if config_path.is_absolute() {
+                config_path
+            } else {
+                std::env::current_dir()?.join(config_path)
+            }
         } else {
             return Ok(Self {
                 state: Static,
@@ -67,7 +60,8 @@ impl RestrictionsRulesReloader {
         match &reloader.state {
             Static => {}
             Config(cfg) => {
-                watcher.watch(&cfg.config_path, notify::RecursiveMode::NonRecursive)?;
+                let parent = cfg.config_path.parent().unwrap_or(&cfg.config_path);
+                watcher.watch(parent, notify::RecursiveMode::NonRecursive)?;
                 *cfg.fs_watcher.lock() = watcher
             }
         }
@@ -97,39 +91,6 @@ impl RestrictionsRulesReloader {
         &self.restrictions
     }
 
-    fn try_rewatch_config(this: RestrictionsRulesReloader, path: PathBuf) {
-        thread::spawn(move || {
-            while !path.exists() {
-                warn!(
-                    "Restrictions config file {:?} does not exist anymore, waiting for it to be created",
-                    path
-                );
-                thread::sleep(Duration::from_secs(10));
-            }
-            let mut watcher = this.state.fs_watcher().lock();
-            let _ = watcher.unwatch(&path);
-            let Ok(_) = watcher
-                .watch(&path, notify::RecursiveMode::NonRecursive)
-                .map_err(|err| {
-                    error!("Cannot re-set a watch for Restriction config file {:?}: {:?}", path, err);
-                    error!("Restriction config file will not be auto-reloaded anymore");
-                })
-            else {
-                return;
-            };
-            drop(watcher);
-
-            // Generate a fake event to force-reload the config
-            let event = notify::Event {
-                kind: EventKind::Create(notify::event::CreateKind::Any),
-                paths: vec![path],
-                attrs: Default::default(),
-            };
-
-            Self::handle_config_fs_event(&this, Ok(event))
-        });
-    }
-
     fn handle_config_fs_event(reloader: &RestrictionsRulesReloader, event: notify::Result<notify::Event>) {
         let this = match &reloader.state {
             Static => return,
@@ -149,19 +110,96 @@ impl RestrictionsRulesReloader {
         }
 
         trace!("Received event: {event:#?}");
-        if let Some(path) = event.paths.iter().find(|p| p.ends_with(&this.config_path)) {
-            match event.kind {
-                EventKind::Create(_) | EventKind::Modify(_) => {
-                    reloader.reload_restrictions_config();
-                }
-                EventKind::Remove(_) => {
-                    warn!("Restriction config file has been removed, trying to re-set a watch for it");
-                    Self::try_rewatch_config(reloader.clone(), path.to_path_buf());
-                }
-                EventKind::Access(_) | EventKind::Other | EventKind::Any => {
-                    trace!("Ignoring event {event:?}");
-                }
+        if event.paths.iter().any(|p| p.ends_with(&this.config_path)) {
+            reloader.reload_restrictions_config();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::Write;
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test_reload_config() -> anyhow::Result<()> {
+        let temp_dir = std::env::temp_dir();
+        let config_name = format!("wstunnel_test_config_{}.yaml", uuid::Uuid::new_v4());
+        let real_path = temp_dir.join(&config_name);
+        let link_path = temp_dir.join(format!("link_{}", config_name));
+
+        let config1 = r#"
+    restrictions:
+      - name: "Allow all"
+        match:
+          - !Any
+        allow:
+          - !Tunnel
+    "#;
+        {
+            let mut file = File::create(&real_path)?;
+            write!(file, "{}", config1)?;
+        }
+
+        #[cfg(unix)]
+        std::os::unix::fs::symlink(&real_path, &link_path)?;
+        #[cfg(windows)]
+        std::os::windows::fs::symlink_file(&real_path, &link_path)?;
+
+        let reloader =
+            RestrictionsRulesReloader::new(RestrictionsRules::from_config_file(&link_path)?, Some(link_path.clone()))?;
+
+        // Verify initial config
+        {
+            let restrictions = reloader.restrictions_rules().load();
+            assert_eq!(restrictions.restrictions.len(), 1);
+            assert_eq!(restrictions.restrictions[0].name, "Allow all");
+        }
+
+        let config2 = r#"
+    restrictions:
+      - name: "Deny all"
+        match:
+          - !Any
+        allow: []
+    "#;
+
+        thread::sleep(Duration::from_millis(100));
+
+        let real_path_2 = temp_dir.join(format!("2_{}", config_name));
+        {
+            let mut file = File::create(&real_path_2)?;
+            write!(file, "{}", config2)?;
+            file.sync_all()?;
+        }
+
+        let temp_link_path = temp_dir.join(format!("temp_link_{}", config_name));
+        #[cfg(unix)]
+        std::os::unix::fs::symlink(&real_path_2, &temp_link_path)?;
+        #[cfg(windows)]
+        std::os::windows::fs::symlink_file(&real_path_2, &temp_link_path)?;
+
+        std::fs::rename(&temp_link_path, &link_path)?;
+
+        // Wait for reload
+        let mut reloaded = false;
+        for _ in 0..50 {
+            thread::sleep(Duration::from_millis(100));
+            let restrictions = reloader.restrictions_rules().load();
+            if restrictions.restrictions[0].name == "Deny all" {
+                reloaded = true;
+                break;
             }
         }
+
+        let _ = std::fs::remove_file(&link_path);
+        let _ = std::fs::remove_file(&real_path);
+        let _ = std::fs::remove_file(&real_path_2);
+        assert!(reloaded, "Configuration was not reloaded");
+
+        Ok(())
     }
 }
